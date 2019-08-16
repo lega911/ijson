c35d755fe263c6fc477979536a4eab7532f5d936
Oleg Nechaev <lega911@gmail.com>
2019-08-16 22:24:19 +0200

    fix nginx conf

--- example/benchmark/uwsgi/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker run -d --network host -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro nginx:1.15
-echo uwsgi --plugin python3 --socket :5000 --wsgi-file app.py --processes=4 --disable-logging
-docker run -it --network host -v `pwd`:/app py
